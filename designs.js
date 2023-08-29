fa117c696c7bd82300f1f756534b0057400c3b13
Kayla Thorne <kthornetdcp@gmail.com>
2023-08-28 21:19:24 -0400

    fixed makegrid function

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
 // Select color input
-// Select size input
+const colorPicker = document.getElementById("colorPicker");
 
-// When size is submitted by the user, call makeGrid()
+
+const pixelCanvas = document.getElementById("pixelCanvas");
+const sizePicker = document.getElementById("sizePicker");
+
+// Select size input
+const inputHeight = document.getElementById("inputHeight");
+const inputWidth = document.getElementById("inputWidth");
+
 
 function makeGrid() {
+    pixelCanvas.innerHTML = '';  //to clear previous grid
 
-// Your code goes here!
+    const rows = inputHeight.value;
+    const cols = inputWidth.value;
+
+    for (let r = 0; r < rows; r++) {
+        let row = pixelCanvas.insertRow(r);
+        for (let c = 0; c < cols; c++) {
+            let cell = row.insertCell(c);
+
+            //each cell has click event
+            cell.addEventListener('click', function() {
+                cell.style.backgroundColor = colorPicker.value;
+            });
+        }
+    };
 
 }
+
+//call makeGrid() when user submits size
+sizePicker.addEventListener('submit', function(event) {
+    event.preventDefault();
+    makeGrid();
+});
